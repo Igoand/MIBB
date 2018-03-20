dd557854f0f00c6d9fc3bcc34f5de69e26635b51
Igoand <igorandrejew@hotmail.com>
2018-03-20 21:28:30 +0100

    Initial commit

---
+++ app/src/main/java/com/ian/igoand/mibb/StartMIBB.java
@@ -0,0 +1,13 @@
+package com.ian.igoand.mibb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartMIBB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_mibb);
+    }
+}
