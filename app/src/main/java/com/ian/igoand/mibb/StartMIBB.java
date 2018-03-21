ec7b4267d31fcdedb5bb4d61bd4093d1f34ec545
Igoand <igorandrejew@hotmail.com>
2018-03-21 20:37:08 +0100

    Testowy commit do GH

--- app/src/main/java/com/ian/igoand/mibb/StartMIBB.java
+++ app/src/main/java/com/ian/igoand/mibb/StartMIBB.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class StartMIBB extends AppCompatActivity {
 
-    // Do testowego pusha
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
