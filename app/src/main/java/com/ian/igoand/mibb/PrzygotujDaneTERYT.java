300e9fb01b18db7a6bd66733141a27237e9eaed7
Igoand <igorandrejew@hotmail.com>
2018-04-17 09:47:30 +0200

    Jako tako działające AutoCompleteInputy. Do weryfikacji problemy z miejscowościami(nie znajduje wielu miejscowości).

--- app/src/main/java/com/ian/igoand/mibb/PrzygotujDaneTERYT.java
+++ app/src/main/java/com/ian/igoand/mibb/PrzygotujDaneTERYT.java
@@ -1,22 +1,20 @@
 package com.ian.igoand.mibb;
 
 import android.content.Context;
-import android.provider.ContactsContract;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PrzygotujDaneTERYT {
 
+    CSVReaderAlternative czytajCSV = new CSVReaderAlternative();
 
 
-    void pobierzDaneTeryt(Context kontekst, String typDanychTeryt){
-        DatabaseHelper mibbDB = new DatabaseHelper(kontekst);
-    }
-
-
-
-    void szukajWojewodztwo(AutoCompleteTextView obiekt, Context kontekst, int zrodlo){
-        String[] nazwyWojewodztwList = {"test"};
+    void szukajWojewodztwo(AutoCompleteTextView obiekt, Context kontekst, int zrodlo) throws IOException {
+        int rawIdWojewodztwa = R.raw.wojewodztwa_import_file;
+        ArrayList<String> nazwyWojewodztwList = (ArrayList<String>) czytajCSV.odczytaneDane(kontekst, rawIdWojewodztwa);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(kontekst, zrodlo, nazwyWojewodztwList);
 
         obiekt.setAdapter(adapter);
@@ -24,8 +22,9 @@ public class PrzygotujDaneTERYT {
         obiekt.setAdapter(adapter);
     }
 
-    void szukajPowiat(AutoCompleteTextView obiekt, Context kontekst, int zrodlo){
-        String[] nazwyPowiatow = {"test"};
+    void szukajPowiat(AutoCompleteTextView obiekt, Context kontekst, int zrodlo) throws IOException {
+        int rawIdPowiaty = R.raw.powiaty_import_file;
+        ArrayList<String> nazwyPowiatow = (ArrayList<String>) czytajCSV.odczytaneDane(kontekst, rawIdPowiaty);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(kontekst, zrodlo, nazwyPowiatow);
 
         obiekt.setAdapter(adapter);
@@ -33,9 +32,20 @@ public class PrzygotujDaneTERYT {
         obiekt.setAdapter(adapter);
     }
 
-    void szukajGmine(AutoCompleteTextView obiekt, Context kontekst, int zrodlo){
-        String[] nazwyGmin = {"test"};
+    void szukajGmine(AutoCompleteTextView obiekt, Context kontekst, int zrodlo) throws IOException {
+        int rawIdGminy = R.raw.gminy_import_file;
+        ArrayList<String> nazwyGmin = (ArrayList<String>) czytajCSV.odczytaneDane(kontekst, rawIdGminy);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(kontekst, zrodlo, nazwyGmin);
+
+        obiekt.setAdapter(adapter);
+        obiekt.setThreshold(4);
+        obiekt.setAdapter(adapter);
+    }
+
+    void szukajMiejscowosc(AutoCompleteTextView obiekt, Context kontekst, int zrodlo) throws IOException {
+        int rawIdMiejscowosci = R.raw.miejscowosci_import_file;
+        ArrayList<String> nazwyMiejscowosci = (ArrayList<String>) czytajCSV.odczytaneDane(kontekst, rawIdMiejscowosci);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(kontekst, zrodlo, nazwyMiejscowosci);
 
         obiekt.setAdapter(adapter);
         obiekt.setThreshold(4);
